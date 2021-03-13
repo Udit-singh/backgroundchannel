7ce9748a9f5979f34c8b0bdb0a14ce97aca111d0
sidharathjain2 <sidharath0367.cse19@chitkara.edu.in>
2021-03-13 22:15:22 +0530

    initial commit

--- android/app/src/main/java/com/example/backgrnd/MainActivity.java
+++ android/app/src/main/java/com/example/backgrnd/MainActivity.java
@@ -28,30 +28,30 @@ public class MainActivity extends FlutterActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // GeneratedPluginRegistrant.registerWith(this);
-        forService=new Intent(MainActivity.this,AlarmReceiver.class);
-//        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
-//            startForegroundService(forService);
-//
-//        }else{
-//            startService(forService);
-//           }
+        forService=new Intent(MainActivity.this,MyService.class);
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
+            startForegroundService(forService);
 
-//        //Setting alarm pending intent
-       alarmIntentRTC = PendingIntent.getBroadcast(this, 101,forService,0);
-//
-//        //getting instance of AlarmManager service
-        alarmManagerRTC = (AlarmManager)this.getSystemService(ALARM_SERVICE);
-//
-//        //Setting alarm to wake up device every day for clock time.
-//        //AlarmManager.RTC_WAKEUP is responsible to wake up device for sure, which may not be good practice all the time.
-//        // Use this when you know what you're doing.
-//        //Use RTC when you don't need to wake up device, but want to deliver the notification whenever device is woke-up
-//        //We'll be using RTC.WAKEUP for demo purpose only
-      long crnttime=System.currentTimeMillis();
-       long tenseconds=1000*10;
-        alarmManagerRTC.set(AlarmManager.RTC_WAKEUP,
-                crnttime+tenseconds, alarmIntentRTC);
-       // NotificationHelper.scheduleRepeatingElapsedNotification(this);
-        //NotificationHelper.enableBootReceiver(this);
+        }else{
+            startService(forService);
+           }
+
+////        //Setting alarm pending intent
+//       alarmIntentRTC = PendingIntent.getBroadcast(this, 101,forService,0);
+////
+////        //getting instance of AlarmManager service
+//        alarmManagerRTC = (AlarmManager)this.getSystemService(ALARM_SERVICE);
+////
+////        //Setting alarm to wake up device every day for clock time.
+////        //AlarmManager.RTC_WAKEUP is responsible to wake up device for sure, which may not be good practice all the time.
+////        // Use this when you know what you're doing.
+////        //Use RTC when you don't need to wake up device, but want to deliver the notification whenever device is woke-up
+////        //We'll be using RTC.WAKEUP for demo purpose only
+//      long crnttime=System.currentTimeMillis();
+//       long tenseconds=1000*10;
+//        alarmManagerRTC.set(AlarmManager.RTC_WAKEUP,
+//                crnttime+tenseconds, alarmIntentRTC);
+//       // NotificationHelper.scheduleRepeatingElapsedNotification(this);
+//        //NotificationHelper.enableBootReceiver(this);
     }
 }
